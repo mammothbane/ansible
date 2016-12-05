bd72a5ca87ff47acde92fc68a3975cd7140870c5
Nathan Perry <avaglir@gmail.com>
2016-12-04 21:25:01 -0500

    add upates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate iron;
+extern crate router;
+extern crate bodyparser;
+extern crate config;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use config::reader::from_file;
+
+use std::path::Path;
+use std::net::SocketAddr;
+
+static mut ADDRESS: Option<SocketAddr> = None;
+
+type UpdateData struct {
+
+};
+
+fn update_handler(req: &mut Request) -> IronResult<Response> {
+    let auth_header = 
+
+
+    let body = req.get::<bodyparser::Json>();
+    println!("{:?}", body);
+    Ok(Response::with((status::Ok, "post")))
+}
+
+fn retrieve_handler(req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "repsonse")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = from_file(Path::new("ansible.conf")).unwrap();
+    let sock_str = format!("0.0.0.0:{}", cfg.lookup_integer32("port").unwrap());
+    let mut router = Router::new();
+
+    router.post("/update", update_handler, "update ip");
+    router.get("/", retrieve_handler, "ip response");
+
+    Iron::new(router).http(&sock_str[..]).unwrap();
 }
